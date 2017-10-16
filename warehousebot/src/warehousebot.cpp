17ba8e072d37699fbdaef8cad4768c35dd091697
Vtn21 <victortn21@hotmail.com>
2017-10-16 08:02:53 -0200

    Created Eclipse project

---
+++ warehousebot/src/warehousebot.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : warehousebot.cpp
+// Author      : Victor T. Noppeney
+// Version     :
+// Copyright   : All rights reserved
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello world!" << endl; // prints Hello world!
+	return 0;
+}
